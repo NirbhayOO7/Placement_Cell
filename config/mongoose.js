45c6f510dc4b0f96f207bd4f1c7176fe7c46a988
nirbhay <yadavnirbhay2@gmail.com>
2023-05-24 02:55:12 +0530

    commit changes in db conneciton to make webapp live

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,7 +6,7 @@ main().catch(err => console.log('Error connecting to MongoDB Placement Cell',err
 // setting up mongoose configuration 
 async function main(){
 
-    await mongoose.connect('mongodb://0.0.0.0:27017/Placement_Cell');
+    await mongoose.connect('mongodb://127.0.0.1:27017/Placement_Cell');
     db = mongoose.connection;
     console.log('Connected to Database :: MongoDB');
 } 
